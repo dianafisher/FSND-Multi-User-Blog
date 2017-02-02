d3cdbae13078d2320a77434ca83d495dd75d4d5c
Diana Fisher <diana.fisher@gmail.com>
2017-02-02 11:24:58 -0800

    Add base html and front page.

--- blog.py
+++ blog.py
@@ -14,13 +14,38 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
 
+class Handler(webapp2.RequestHandler):
 
-app = webapp2.WSGIApplication([('/', MainPage),
-                               ('/blog/?', BlogFront),
-                               ('/blog/([0-9]+)', PostPage),
-                               ('/blog/newpost', NewPost),
-                               ('/signup', Register),
-                               ('/login', Login),
-                               ('/logout', Logout),
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+### Entities
+
+class Post(db.Model):
+    title = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
+
+    def render(self):
+        # replace new line characters with breaks
+        self.__render_text = self.content.replace('\n', '<br>')
+        return render('post.html', p = self)
+
+class FrontPage(Handler):
+
+    def get(self):
+        self.render("front.html")
+
+
+
+app = webapp2.WSGIApplication([('/', FrontPage),
                                ],
                               debug=True)
